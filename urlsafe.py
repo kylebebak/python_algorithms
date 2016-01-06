46d1bd8587b7014308fd26860ff6d7b4f31685d9
Kyle Bebak <kylebebak@gmail.com>
2016-01-05 21:04:11 -0600

    replaced urlsafe.py with urlsafe.py from geohash package

--- urlsafe.py
+++ urlsafe.py
@@ -1,17 +1,26 @@
+"""
+A module for converting between
+integer <--> urlsafe-string:
+
+Author:
+http://stackoverflow.com/users/64474/miles
+"""
+
 import string
-ALPHABET = string.ascii_lowercase + string.ascii_uppercase + \
-           string.digits + '-_'
+
+# base64url
+ALPHABET = string.ascii_uppercase + string.ascii_lowercase + \
+    string.digits + '-_'
+
+# standard geohash base 32
+# ALPHABET = string.digits + 'bcdefghjkmnpqrstuvwxyz'
+
 ALPHABET_REVERSE = dict((c, i) for (i, c) in enumerate(ALPHABET))
 BASE = len(ALPHABET)
 SIGN_CHARACTER = '$'
 
-"""
-Nice code written by this guy:
-http://stackoverflow.com/users/64474/miles
-"""
-
-def to_base_64(n):
-    """Converts integer to base 64 string."""
+def to_urlsafe(n):
+    """Converts integer to url-safe string."""
     if n < 0:
         return SIGN_CHARACTER + num_encode(-n)
     s = []
@@ -22,8 +31,8 @@ def to_base_64(n):
             break
     return ''.join(reversed(s))
 
-def from_base_64(s):
-    """Converts base 64 string to integer."""
+def from_urlsafe(s):
+    """Converts base url-safe string to integer."""
     if s[0] == SIGN_CHARACTER:
         return -num_decode(s[1:])
     n = 0
